fbf7d45865c4b2915efa73f1dbeead7be7d834a8
Akarsh Ache <55279661+akarsh27@users.noreply.github.com>
2020-08-02 13:05:27 +0530

    Create LoadingData.py

---
+++ LoadingData.py
@@ -0,0 +1,10 @@
+import io
+from google.colab import files
+
+train_tsv = files.upload()
+airlines_train = pd.read_csv(io.BytesIO(train_tsv['Tweets.csv']))
+airlines_train = airlines_train.reindex(np.random.permutation(airlines_train.index))
+airlines_train = airlines_train[['text', 'airlines_sentiment']]
+
+sns.factorplot(x="airline_sentiment", data=airlines_train, kind="count", size=6, aspect=1.5, palette="GnBu_r")
+plt.show();
