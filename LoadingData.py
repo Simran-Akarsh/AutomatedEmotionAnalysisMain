7dfca03244e92564abf12b04a6103eb78e2d623b
Simran Agarwal <55496763+smileysim01@users.noreply.github.com>
2020-08-11 22:45:11 +0530

    Update LoadingData.py

--- LoadingData.py
+++ LoadingData.py
@@ -4,7 +4,7 @@ from google.colab import files
 train_tsv = files.upload()
 airlines_train = pd.read_csv(io.BytesIO(train_tsv['Tweets.csv']))
 airlines_train = airlines_train.reindex(np.random.permutation(airline_train.index))
-airlines_train = airlines_train[['text', 'airlines_sentiment']]
+airlines_train = airlines_train[['text', 'airline_sentiment']]
 
 sns.factorplot(x="airline_sentiment", data=airlines_train, kind="count", size=6, aspect=1.5, palette="GnBu_r")
 plt.show();
