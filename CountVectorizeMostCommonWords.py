592f18d368aac805260e5b1c4e9f231571f792a1
Akarsh Ache <55279661+akarsh27@users.noreply.github.com>
2020-08-11 23:07:10 +0530

    Update CountVectorizeMostCommonWords.py

--- CountVectorizeMostCommonWords.py
+++ CountVectorizeMostCommonWords.py
@@ -1,8 +1,8 @@
 cv = CountVectorizer()
 bow = cv.fit_transform(sr_clean)
-word_freq = dict(zip(cv_get_feature_names(), np.asarray(bow.sum(axis=0)).ravel()))
+word_freq = dict(zip(cv.get_feature_names(), np.asarray(bow.sum(axis=0)).ravel()))
 word_counter = collections.Counter(word_freq)
-word_counter_df = pd.DataFrame(word_counter.most_common(20),colums = ['word','freq'])
+word_counter_df = pd.DataFrame(word_counter.most_common(20),columns = ['word','freq'])
 fig, ax = plt.subplots(figsize=(12,10))
 sns.barplot(x="word", y="freq", data=word_counter_df, palette="PuBuGn_d" , ax=ax)
 plt.show();
