d70e0def258e07e42b08897565f86cdde30699ae
Simran Agarwal <55496763+smileysim01@users.noreply.github.com>
2020-08-20 18:30:12 +0530

    Update UsingCleanText.py

--- UsingCleanText.py
+++ UsingCleanText.py
@@ -1,11 +1,11 @@
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
 ct = CleanText()
-sr_clean = ct.fit_transform(airlines_train.text)
+
+sr_clean = ct.fit_transform(emotions_train.content)
+
 sr_clean.sample(5)
+
 empty_clean = sr_clean == ''
-print('{} records have no words left after texting cleaning'.format(sr_clean[empty_clean].count()))
+
+print('{} records have no words left after text cleaning'.format(sr_clean[empty_clean].count()))
+
 sr_clean.loc[empty_clean] = '[no_text]'
-
-
