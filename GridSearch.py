0776078adbe1ebdd81af5e18626f8cad56c6baf5
Simran Agarwal <55496763+smileysim01@users.noreply.github.com>
2020-08-20 18:33:31 +0530

    Update GridSearch.py

--- GridSearch.py
+++ GridSearch.py
@@ -1,42 +1,55 @@
-#Based on http://scikit-learn.org/stable/auto_examples/model_selection/grid_search_text_feature_extraction.html
+# Based on http://scikit-learn.org/stable/auto_examples/model_selection/grid_search_text_feature_extraction.html
 def grid_vect(clf, parameters_clf, X_train, X_test, parameters_text=None, vect=None, is_w2v=False):
-    textcountscols = ['count_capitl_words','count_emojis','count_excl_quest_marks','count_hashtags', 'count_mentions', 'count_urls', 'count_words']
-
+    
+    textcountscols = ['count_capital_words','count_emojis','count_excl_quest_marks','count_hashtags'
+                      ,'count_mentions','count_urls','count_words']
+    
     if is_w2v:
         w2vcols = []
         for i in range(SIZE):
             w2vcols.append(i)
-        features = FeatureUnion([('textcounts', ColumnExtractor(cols=textcountscols)), ('pipe', Pipeline([('cleantext', ColumnExtractor(cols='clean_text')), ('vect', vect)]))], n_jobs=-1)
+        features = FeatureUnion([('textcounts', ColumnExtractor(cols=textcountscols))
+                                 , ('w2v', ColumnExtractor(cols=w2vcols))]
+                                , n_jobs=-1)
     else:
-        features = FeatureUnion([('textcounts', ColumnExtractor(cols=textcountscols)), ('pipe', Pipeline([('cleantext', ColumnExtractor(cols='clean_text')), ('vect', vect)]))], n_jobs=-1)
+        features = FeatureUnion([('textcounts', ColumnExtractor(cols=textcountscols))
+                                 , ('pipe', Pipeline([('cleantext', ColumnExtractor(cols='clean_text')), ('vect', vect)]))]
+                                , n_jobs=-1)
 
-    pipeline = Pipeline([('features', features), ('clf', clf)])  
-
-  #Join the parameters dictionaries together
+    
+    pipeline = Pipeline([
+        ('features', features)
+        , ('clf', clf)
+    ])
+    
+    # Join the parameters dictionaries together
     parameters = dict()
     if parameters_text:
         parameters.update(parameters_text)
-    parameters.update(parameters_clf)   #Make sure you have scikit-learn version 0.19 or higher to use multiple scoring metrics
-    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1, cv=5)
+    parameters.update(parameters_clf)
 
+    # Make sure you have scikit-learn version 0.19 or higher to use multiple scoring metrics
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1, cv=5)
+    
     print("Performing grid search...")
-    print("pipeline:", [name for name, _ in pipleline.steps])
+    print("pipeline:", [name for name, _ in pipeline.steps])
     print("parameters:")
     pprint(parameters)
-    tO = time
+
+    t0 = time()
     grid_search.fit(X_train, y_train)
-    print("done in %0.3fs" % (time() - tO))
+    print("done in %0.3fs" % (time() - t0))
     print()
-    print("Best CV score: %0.3f" % grid_search.bestscore_)
+
+    print("Best CV score: %0.3f" % grid_search.best_score_)
     print("Best parameters set:")
     best_parameters = grid_search.best_estimator_.get_params()
     for param_name in sorted(parameters.keys()):
         print("\t%s: %r" % (param_name, best_parameters[param_name]))
-
-    print("Test score with best_estimator_: %0.3f" % grid_search.best_estimator_.score(X_test, y_test))  
+        
+    print("Test score with best_estimator_: %0.3f" % grid_search.best_estimator_.score(X_test, y_test))
     print("\n")
     print("Classification Report Test Data")
     print(classification_report(y_test, grid_search.best_estimator_.predict(X_test)))
-
+                        
     return grid_search
-
