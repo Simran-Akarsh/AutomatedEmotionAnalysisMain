65caadcbfd59e6513a808080df7ac0ef7a2a9709
Simran Agarwal <55496763+smileysim01@users.noreply.github.com>
2020-08-20 18:12:34 +0530

    Update ImportingLibraries.py

--- ImportingLibraries.py
+++ ImportingLibraries.py
@@ -1,18 +1,18 @@
-!pip3 install emoji
-import numpy as np
 import pandas as pd
-pd.set_option('display.max_colwidth', -1)
-from time import time
+import numpy as np
+from google.colab import files 
+import io
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from bs4 import BeautifulSoup
 import re
-import string
-import os
-import emoji
-from pprint import pprint
-import collections
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set(style="darkgrid")
-sns.set(font_scale=1.3)
+
+nltk.download("punkt")
+nltk.download("stopwords")
+
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.svm import LinearSVC
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -23,10 +23,25 @@ from sklearn.metrics import classification_report
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.externals import joblib
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, learning_curve
+
+!pip3 install emoji
+pd.set_option('display.max_colwidth', -1)
+from time import time
+import re
+import string
+import os
+import emoji
+from pprint import pprint
+import collections
+
+import seaborn as sns
+sns.set(style="darkgrid")
+sns.set(font_scale=1.3)
+
 import gensim
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
 import warnings
 warnings.filterwarnings('ignore')
 np.random.seed(37)
